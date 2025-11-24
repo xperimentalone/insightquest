19e225f60c07bd5cc4e653c0d2aef8da77680638
xperimentalone <sklaisuki@gmail.com>
2025-11-25 00:13:32 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Replace '<your-repo-name>' with the name of your GitHub repository.
-  // For example, if your repository URL is https://github.com/user/my-app,
-  // the base should be '/my-app/'.
-  base: '/insightquest/',
+  server: {
+    host: '0.0.0.0',
+    port: 5000,
+    hmr: {
+      host: process.env.REPLIT_DEV_DOMAIN,
+      clientPort: 443,
+      protocol: 'wss'
+    }
+  },
   define: {
-    // This makes the environment variable available in your client-side code
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   }
 })
